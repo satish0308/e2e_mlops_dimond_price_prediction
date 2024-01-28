586b8286ddb19ffc558c8b5910ef17e3d339ce87
satish <hiremath0308@gmail.com>
2024-01-28 14:55:47 +0530

    template

---
+++ setup.py
@@ -0,0 +1,47 @@
+import setuptools
+from typing import List
+from pathlib import Path
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''this Fucntion will return list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+__version__ = "0.0.1"
+
+REPO_NAME = "e2e_mlops_dimond_price_prediction"
+AUTHOR_USER_NAME = "satish0308"
+SRC_REPO = "Dimond-Price-Prediction"
+AUTHOR_EMAIL = "hiremath0308@gmail.com"
+
+
+
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Diamond-Price-Prediction Model",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=get_requirements('requirements.txt'),
+)
---
+++ template.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+#from src.CNNClassifier.logger import logging
+import logging
+
+logging.basicConfig(
+      level=logging.INFO,
+      format='[%(asctime)s]:%(message)s:'
+
+ )
+
+project_name ="Diamond Price Predictor"
+
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    ".github/workflows/ci.yaml",
+    ".github/workflows/product-release.yaml",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/logger.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "init_setup.sh",     
+     "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiments/experiments.ipynb",
+    "config/config.yaml",
+    "dvc.yaml",
+    "parmas.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb",
+    "templates/index.html",
+    "main.py"
+]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    file_dir,file_name=os.path.split(filepath)
+    if file_dir!="":
+        os.makedirs(file_dir,exist_ok=True)
+        logging.info (f"creating the file Directory{file_dir} for the file name :{file_name} ")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open (filepath,'w') as f:
+            pass
+            logging.info(f'creating empty file :{file_name}')
+    else:
+        logging.info(f"{file_name} already exist")
