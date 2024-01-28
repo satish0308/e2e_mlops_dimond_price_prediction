62f7899a077a982390ae6deb3b2c9a847a72186f
satish <hiremath0308@gmail.com>
2024-01-28 20:10:33 +0530

    corrected instal require file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''this Fucntion will return list of requirements'''
     requirements=[]
-    with open(file_path) as file_obj:
+    with open(Path(file_path)) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace('\n',"") for req in requirements]
         if HYPEN_E_DOT in requirements:
@@ -43,5 +43,5 @@ setuptools.setup(
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    install_requires=get_requirements('requirements.txt'),
+    install_requires=get_requirements('requirements.txt')
 )
